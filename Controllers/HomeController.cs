d0d1d7a7c9a473ff0a49fbded9a19a8b7dc7bf4f
corbanpro <corbanpro@gmail.com>
2023-02-27 14:45:40 -0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WaterProject.Models;
+
+namespace WaterProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IWaterProjectRepository repo;
+
+
+        public HomeController(IWaterProjectRepository temp)
+        {
+            repo = temp;
+        }
+
+        public IActionResult Index()
+        {
+
+            var blah = repo.Projects.ToList();
+
+            return View(blah);
+        }
+    }
+}
